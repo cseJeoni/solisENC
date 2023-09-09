bd67f170b23f49707f91474af78f229c4959ec17
cseJeoni <cse.jeoni@gmail.com>
2023-09-09 17:04:25 +0900

    change into blue

--- kr/business/business.js
+++ kr/business/business.js
@@ -6,13 +6,13 @@ function viewField(field) {
     if (field == 1) {
         document.getElementById("field-section1").style.display ='block';		
         document.getElementById("field-section2").style.display ='none';
-        document.getElementsByClassName("field1")[0].style.backgroundColor="#cc1616";
+        document.getElementsByClassName("field1")[0].style.backgroundColor="#44a2fa";
         document.getElementsByClassName("field2")[0].style.backgroundColor="gray";
     } else if (field == 2) {
         document.getElementById("field-section1").style.display ='none';		
         document.getElementById("field-section2").style.display ='block';
         document.getElementsByClassName("field1")[0].style.backgroundColor="gray";
-        document.getElementsByClassName("field2")[0].style.backgroundColor="#cc1616";
+        document.getElementsByClassName("field2")[0].style.backgroundColor="#44a2fa";
     }
     
 }
